b9108a5bd96b0ccd75e369f304346649436a9ac1
João Carlos <joaocxs10@gmail.com>
2024-08-05 09:18:39 -0300

    altereações na consulta para depuração local

--- server.mjs
+++ server.mjs
@@ -18,7 +18,7 @@ const fetchWithTimeout = (url, options, timeout = 5000000) => {
 
 app.post('/consulta', async (req, res) => {
     const { tipoConsulta, apiKey, parametros } = req.body;
-    const url = `https://api.conciliadora.com.br/api/Consulta${tipoConsulta}?$filter=${parametros}`;
+    const url = `https://api.conciliadora.com.br/api/Consulta${tipoConsulta}?$filter=${encodeURIComponent(parametros)}`;
 
     console.log(`URL: ${url}, API Key: ${apiKey}`);
 
