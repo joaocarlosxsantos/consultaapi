380b5ec443f96d15a884cb9933694ac8c88761aa
João Carlos <joaocxs10@gmail.com>
2024-08-05 09:30:03 -0300

    correção nas consultas

--- public/script.js
+++ public/script.js
@@ -7,12 +7,13 @@ document.getElementById('consultaForm').addEventListener('submit', async functio
     const data_fim = document.getElementById('data_fim').value;
     const estabelecimento = document.getElementById('estabelecimento').value;
     const nsu = document.getElementById('nsu').value;
+    const parametros = `Data${tipoConsulta} ge ${data_inicio} and Data${tipoConsulta} le ${data_fim}`;
+    //let data_inicio_formatada = `${data_inicio.getFullYear()}-${(data_inicio.getMonth() + 1).toString().padStart(2, '0')}-${data_inicio.getDate().toString().padStart(2, '0')}`;
+        //let data_fim_formatada = `${data_fim.getFullYear()}-${(data_fim.getMonth() + 1).toString().padStart(2, '0')}-${data_fim.getDate().toString().padStart(2, '0')}`;
+
 
     try {
-        //let data_inicio_formatada = `${data_inicio.getFullYear()}-${(data_inicio.getMonth() + 1).toString().padStart(2, '0')}-${data_inicio.getDate().toString().padStart(2, '0')}`;
-        //let data_fim_formatada = `${data_fim.getFullYear()}-${(data_fim.getMonth() + 1).toString().padStart(2, '0')}-${data_fim.getDate().toString().padStart(2, '0')}`;
-        let parametros = ('Data',tipoConsulta,'ge',data_inicio,'and Data',tipoConsulta,'le',data_fim)
-
+    
         const response = await fetch('/consulta', {
             method: 'POST',
             headers: {
